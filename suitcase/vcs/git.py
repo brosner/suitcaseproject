b4eca9a41c14236cec56aa7ef7ead0f296da542a
Robert Lofthouse <daaifung@googlemail.com>
2009-08-07 13:59:39 +0100

    Added git support. Fixed typo in bazaar.py and made a generic package maintainer name for suitcase.yml

--- suitcase/vcs/git.py
+++ suitcase/vcs/git.py
@@ -1,4 +1,4 @@
-"""Bazaar helpers"""
+"""Git helpers"""
 
 import commands
 import os
@@ -7,16 +7,17 @@ import re
 from suitcase.exceptions import SuitcaseVcsError
 from suitcase.vcs.base import VcsBase
 
-class Bazaar(VcsBase):
+class Git(VcsBase):
     
-    """Class for handling the interface to the bazaar vcs"""
+    """Class for handling the interface to the git vcs"""
     
     def get_directory_revision(self, directory):
     
         """Works on the basis of looking up from the local checkout"""
-        result = commands.getstatusoutput("bzr log -q -l 1 %s" % directory)
+        os.chdir(directory)
+        result = commands.getstatusoutput("git log -1")
         if result[0] > 0:
-            raise SuitcaseVcsError("Can't find bzr version for %s (%s)\n"\
+            raise SuitcaseVcsError("Can't find git version for %s (%s)\n"\
                 "Command exited with error (%s), %s" % (
                     directory, 
                     os.getcwd(), 
@@ -26,10 +27,10 @@ class Bazaar(VcsBase):
             )
             
         else:
-            match = re.search(r"^revno: (\d+)$", result[1], re.M)
+            match = re.search(r"^commit ([A-Za-z0-9_-]+)", result[1], re.M)
             if match is None:
                 raise SuitcaseVcsError(
-                    "Can't find bzr version for %s" % directory
+                    "Can't find git version for %s" % directory
                 )
                 
             rev = match.group(1)
